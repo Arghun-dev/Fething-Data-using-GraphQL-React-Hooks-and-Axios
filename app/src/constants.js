0e0e0104dd3aa9d3ee7435cbd6174f1a1f7de936
Arghun-dev <arghun.developer@gmail.com>
2021-02-24 01:14:42 +0330

    second commit

---
+++ app/src/constants.js
@@ -0,0 +1,15 @@
+export const GRAPHQL_API = "http://localhost:8080/graphql";
+
+export const GET_VIDEOS_QUERY = `
+  query videos {
+    videos {
+      id
+      title
+      url
+      author {
+        id
+        name
+      }
+    }
+  }
+`;
